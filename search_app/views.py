cd83654120f85a79974f38bd29e091e1fec9db27
DharamveerSinghGrewal <135950075+DharamveerGrewal@users.noreply.github.com>
2025-01-19 19:57:11 -0500

    Integrate indexing and query logic with Django views

--- search_app/views.py
+++ search_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from indexing.query import search
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        results = search(query)
+        return render(request, 'search_app/index.html', {'results': results, 'query': query})
+    return render(request, 'search_app/index.html')
