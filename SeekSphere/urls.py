6ba6acbfe45f04180ebbf6b9c6cb27fc5fc1e926
DharamveerSinghGrewal <135950075+DharamveerGrewal@users.noreply.github.com>
2025-01-19 21:00:13 -0500

    Add search results display to frontend

--- SeekSphere/urls.py
+++ SeekSphere/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from search_app import views  # Import views from your app
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # Add a route for the homepage
 ]
--- search_app/views.py
+++ search_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from indexing.query import search
 
 def index(request):
